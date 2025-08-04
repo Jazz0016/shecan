32acb2d5957c118b7bbecd740790cd2dc75b6642
Jazz0016 <140640466+Jazz0016@users.noreply.github.com>
2025-08-05 00:07:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   };
 
   const handleLogin = async () => {
-    const res = await fetch('http://localhost:5000/api/login', {
+    const res = await fetch('https://shecan-backend-jessica-jasmine.onrender.com', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(formData),
